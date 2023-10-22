20c8d13cc5ae9843ac120c342431109b8d837334
ProjectARIMA <146095147+ProjectARIMA@users.noreply.github.com>
2023-10-22 17:09:28 +0500

    Update app.py

--- app.py
+++ app.py
@@ -10,36 +10,45 @@ from statsmodels.tsa.arima.model import ARIMA
 from sklearn.ensemble import IsolationForest
 
 # Define the Streamlit app
-st.title("Anomaly Detection and Time Series Forecasting Web Application")
-st.set_option('deprecation.showPyplotGlobalUse', False)
+st.title("Anomaly Detection and Time Series Forecasting Web Application") #It sets the website title to 'Anomaly Detection and Time Series Forecasting Web Application'
+
+st.set_option('deprecation.showPyplotGlobalUse', False) #It hides all the warnings in python output
 
 # Upload dataset
-uploaded_file = st.file_uploader("Upload a CSV file", type=["csv"])
+uploaded_file = st.file_uploader("Upload a CSV file", type=["csv"]) # A widget to let users input his/her CSV files.
 if uploaded_file is not None:
-    data = pd.read_csv(uploaded_file, parse_dates=['Month'])
+    data = pd.read_csv(uploaded_file, parse_dates=['Month']) # Creates a panda's DataFrame object and loads the contents of CSV file in it, and considers the 'month' column as dates. 
 
     # Data Preprocessing
-    st.subheader("Data Overview")
-    data_display = data.head().copy()
-    data_display.columns = ['1', '2']
-    data_display.index = ['' for _ in range(len(data_display))]  # Set index names to empty strings
-    st.write(data_display)
+    st.subheader("Data Overview") # Sets the subheader to 'Data Overview'
+    
+    data_display = data.head().copy() # Creates a DataFrame object and stores the first few rows of data.
+    
+    data_display.columns = ['1', '2'] # Changes the column name of 'data_display' to '1' and '2' for simplification.
+    
+    data_display.index = ['' for _ in range(len(data_display))]  # Set index names to empty strings. Index names/values are unique labels attached to each row of the DataFrame.
+    
+    st.write(data_display) # Write's the DataFrame object as table onto the website.
 
     # Function to check stationarity using ADF Test
     def check_stationarity(passengers):
-        result = adfuller(passengers)
-        adf_statistic = result[0]
-        p_value = result[1]
+        result = adfuller(passengers) # adfuller() checks whether data is stationary or not and returns an array containing values such as statistic, p-value etc.
+        
+        adf_statistic = result[0] # Returns the test statistic value.
+        
+        p_value = result[1] # Returns the p-value.
 
-        st.subheader("ADF Test for Stationarity")
-        st.write('ADF Statistic:', adf_statistic)
-        st.write('p-value:', p_value)
+        st.subheader("ADF Test for Stationarity") # Prints 'ADF Test for Stationarity' as subheading on the website.
+        
+        st.write('ADF Statistic:', adf_statistic) # Prints the test statistic value with label 'ADF Statistic:' on the website.
+        
+        st.write('p-value:', p_value) # Prints the test statistic p-value with label 'p-value:' on the website.
 
     # Function to plot time series
     def plot_time_series_with_index(time_index, passengers, title):
         st.subheader(title)
         plt.figure(figsize=(10, 6))
-        plt.plot(time_index, passengers)
+        plt.plot(time_index, passengers) # Plot values of 'time_index' and 'passenger' in combination of the form of (x,y) coordinaties.
         plt.xlabel('')
         plt.ylabel('')
         plt.title(title)
@@ -47,20 +56,35 @@ if uploaded_file is not None:
 
     # Function to perform seasonal decomposition
     def decompose_seasonality(passengers):
-        decomposition = seasonal_decompose(passengers, model='additive', period=12)
+        decomposition = seasonal_decompose(passengers, model='additive', period=12) # seasonal_decompose() returns an object having trend, seasonal, and residual as attributes. 
+                                                                                    # 'passenger' represents Series object containing passenger column data.
+                                                                                    # 
+                                                                                    # 'period' = 12 means that the repetition of patterns are expected to be yearly.
+        
         trend = decomposition.trend
         seasonal = decomposition.seasonal
         residual = decomposition.resid
 
         st.subheader("Seasonal Decomposition")
         st.write("Original Data:")
-        plot_time_series_with_index(data['Month'], passengers, "Original Data")
+        plot_time_series_with_index(data['Month'], passengers, "Original Data") # Passing data['month'] Series object as index values.
+                                                                                # Passing 'passengers' Series object.
+                                                                                # Passing 'Original Data' as title.
+        
         st.write("Trend Component:")
-        plot_time_series_with_index(data['Month'], trend, "Trend Component")
+        plot_time_series_with_index(data['Month'], trend, "Trend Component") # Passing data['month'] Series object as index values.
+                                                                             # Passing 'trend' Series object containing trend component values.
+                                                                             # Passing 'Trend Component' as title.
+        
         st.write("Seasonal Component:")
-        plot_time_series_with_index(data['Month'], seasonal, "Seasonal Component")
+        plot_time_series_with_index(data['Month'], seasonal, "Seasonal Component") # Passing data['month'] Series object as index values.
+                                                                                   # Passing 'seasonal' Series object containing seasonal component values.
+                                                                                   # Passing 'Seasonal Component' as title.
+        
         st.write("Residual Component:")
-        plot_time_series_with_index(data['Month'], residual, "Residual Component")
+        plot_time_series_with_index(data['Month'], residual, "Residual Component")  # Passing data['month'] Series object as index values.
+                                                                                    # Passing 'residual' Series object containing residual component values.
+                                                                                    # Passing 'Residual Component' as title.
 
     # Function to remove anomalies using Isolation Forest
     def remove_anomalies(passengers):
@@ -110,7 +134,7 @@ if uploaded_file is not None:
         st.pyplot()
 
     
-    check_stationarity(data['#Passengers'])
+    check_stationarity(data['#Passengers']) # Creates a Series object of '#Passengers' and pass it as an argument.
     decompose_seasonality(data['#Passengers'])
     remove_anomalies(data['#Passengers'])
 
